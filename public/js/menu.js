9d998c2a5d1a45513ae5281b8224f2cbfbc2382d
unknown <rmonnerat.pro@gmail.com>
2016-04-05 22:44:41 +0200

    Load widget into menu

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,27 +1,41 @@
-$(document).ready(function() {
+function loadWidget(img, render_page, callback) {
+	$(".swiper-wrapper").prepend(
+	"<img class='swiper-slide' src='" + img
+	+ "' data-render='" + render_page
+	+ "' data-callback='" + callback
+	+ "' />");
+	
 	var mySwiper = new Swiper ('.swiper-container', {
 		// Optional parameters
 		direction: 'horizontal',
 		loop: true,
-		
+
 		// If we need pagination
 		pagination: '.swiper-pagination',
-		
+
 		// Navigation arrows
 		nextButton: '.swiper-button-next',
 		prevButton: '.swiper-button-prev',
-		
+
 		preloadImages: true,
 		effect: 'coverflow',
-			centeredSlides: true,
-			slidesPerView: 3,
-			coverflow: {
-				rotate: 50,
-				
-				depth: 100,
-				modifier: 1,
-				slideShadows: false,
-				stretch: 0,
-			}
-	  });
+		centeredSlides: true,
+		slidesPerView: 3,
+		coverflow: {
+			rotate: 50,
+
+			depth: 100,
+			modifier: 1,
+			slideShadows: false,
+			stretch: 0,
+		}
+	});
+}
+
+$(document).ready(function() {
+	loadWidget("img/map.png", "test.html", "xxxx");
+	loadWidget("img/drive_photo.png", "test.html", "xxxx");
+	loadWidget("img/news.png", "test.html", "xxxx");
+	loadWidget("img/cinema.png", "test.html", "xxxx");
+	loadWidget("img/agario.png", "test.html", "xxxx");
 });
