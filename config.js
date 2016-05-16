da3cafc7d3e7109aa0480fa176b1409ba21cd475
Shinao <rmonnerat.pro@gmail.com>
2016-05-16 12:17:16 +0200

    Save and load access token for Drive

--- config.js
+++ config.js
@@ -4,15 +4,17 @@ config.widget = {};
 config.widget.cinema = {};
 config.widget.news = {};
 config.widget.weather = {};
+config.widget.photo = {};
 config.web = {};
 
 config.widget.cinema.url = 'http://www.allocine.fr/seance/salle_gen_csalle=C0159.html';
 config.widget.cinema.refreshRateInMinutes = 60;
-config.widget.weather.appid = 'db799de8cb3ae16e1528492664ea512a';
+config.widget.weather.appid = 'ApiKeyHere';
 config.widget.weather.url = 'http://api.openweathermap.org/data/2.5/weather?id=2988507&units=metric&appid=' + config.widget.weather.appid;
 config.widget.weather.refreshRateInMinutes = 10;
 config.widget.news.url =  'http://www.france24.com/en/top-stories/rss';
 config.widget.news.refreshRateInMinutes = 60;
+config.widget.photo.refreshToken = 'RefreshTokenHere';
 
 config.web.port = 3000;
 config.web.motionUrl = 'http://localhost:3001/takePhoto';
--- server.js
+++ server.js
@@ -24,8 +24,89 @@ app.get('/cinema', widgets.cinema);
 // Web Socket IO for sending gestures to client
 socketIO.of('/motion').on('connection', function(socket){ socketClient = socket; });
 // Forwarding between motion server and client
-app.get('/motion/gesture', function (req, res) { try { socketClient.emit('gesture', req.query); } catch(e) {} res.sendStatus(200); })
-app.get('/motion/takePhoto', function (req, res) { request(config.web.motionUrl, function(err) {}); res.sendStatus(200); })
+app.get('/motion/gesture', function (req, res) { try { socketClient.emit('gesture', req.query); } catch(e) {} res.sendStatus(200); });
+app.get('/motion/takePhoto', function (req, res) { request(config.web.motionUrl, function(err) {}); res.sendStatus(200); });
+app.get('/motion/uploadPhoto', function (req, res) { 
+  'use strict';
+
+  var fs = require('fs');
+  var google = require('googleapis');
+  var drive = google.drive('v2');
+  var OAuth2Client = google.auth.OAuth2;
+
+  // Client ID and client secret are available at
+  // https://code.google.com/apis/console
+  var CLIENT_ID = "625168260868-d7khqbuva1m80s46a7rv8cglkpko7lnb.apps.googleusercontent.com";
+  var CLIENT_SECRET = "cfN3dYpE5PyRI41v_r2an14J";
+  var REDIRECT_URL = 'http://whatever.com/oauth';
+
+  var oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+  
+  // generate a url that asks permissions for Google+ and Google Calendar scopes
+// var scopes = [
+//   'https://www.googleapis.com/auth/drive',
+// ];
+
+// var url = oauth2Client.generateAuthUrl({
+//   access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
+//   scope: 'https://www.googleapis.com/auth/drive' // If you only need one scope you can pass it as string
+// });
+
+// res.send("<a href="+url+">Click here to get code</a>");
+  
+  oauth2Client.getToken(config.widget.photo.refreshToken, function(err, tokens) {
+  // Now tokens contains an access_token and an optional refresh_token. Save them.
+  if(!err) {
+    oauth2Client.setCredentials(tokens);
+    console.log("Token retrieved: ");
+    console.log(tokens);
+    fs.writeFile("./drive_access_token.txt", tokens.refresh_token);  
+  }
+  else
+    console.log("ERROR getToken: " + err);
+    
+  var saved_access_token = '';
+  try { saved_access_token = fs.readFileSync('./drive_access_token.txt').toString(); }
+  catch (e) { console.log("Error getting access token"); }
+    
+  oauth2Client.setCredentials({
+    access_token: saved_access_token,
+    refresh_token: config.widget.photo.refreshToken
+  });
+    
+    // insertion example
+  drive.files.insert({
+    resource: {
+      title: 'Test',
+      mimeType: 'text/plain'
+    },
+    media: {
+      mimeType: 'text/plain',
+      body: 'Hello World updated with metadata'
+    },
+    auth: oauth2Client
+    }, function (err, response) {
+    console.log('error:', err, 'inserted:', response);
+  });
+});
+  
+  // oauth2Client.refreshAccessToken(function(err, tokens){ console.log(err); console.log("//"); console.log(tokens);});
+
+
+  // drive.files.insert({
+  //   resource: {
+  //     title: 'testimage.jpg',
+  //     mimeType: 'image/jpg'
+  //   },
+  //   media: {
+  //     mimeType: 'image/jpg',
+  //     body: fs.createReadStream('frame.jpg') // read streams are awesome!
+  //   },
+  //   auth: oauth2Client
+  // }, function (err, response) {
+  //   console.log('error:', err, 'inserted:', response);
+  // });
+});
 
 http.listen(config.web.port, function () {
   console.log('Server listening on port %d', config.web.port);
